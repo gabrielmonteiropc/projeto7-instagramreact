9bebd66bdc797931daf67ef6cb995b21e0cf71c8
Gabriel Dos Santos Monteiro <bhcmonteiro54@gmail.com>
2023-07-01 20:09:04 -0300

    requisito de array de posts terminado

--- src/Posts.js
+++ src/Posts.js
@@ -1,77 +1,76 @@
+function ArrayPost(props) {
+    return (
+        <div class="post">
+            <div class="topo">
+                <div class="usuario">
+                    <img src={props.imgPerfil} alt="meowed" />
+                    {props.nomeDoUsuario}
+                </div>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
+
+            <div class="conteudo">
+                <img src={props.imgPost} alt="gato-telefone" />
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <div>
+                        <ion-icon name="bookmark-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="curtidas">
+                    <img src={props.imgcurtidoPor} alt="respondeai" />
+                    <div class="texto">
+                        Curtido por <strong>{props.curtidoPorTexto}</strong> e <strong>outras {props.quantidadeLikes} pessoas</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export default function Posts() {
+
+    const postsArray = [
+        {
+            nomeDoUsuario: "meowed",
+            imgPerfil: "assets/img/meowed.svg",
+            imgPost: "assets/img/gato-telefone.svg",
+            imgcurtidoPor: "assets/img/respondeai.svg",
+            curtidoPorTexto: "respondeai",
+            quantidadeLikes: 101.523
+        },
+        {
+            nomeDoUsuario: "meowed",
+            imgPerfil: "assets/img/barked.svg",
+            imgPost: "assets/img/dog.svg",
+            imgcurtidoPor: "assets/img/adorable_animals.svg",
+            curtidoPorTexto: "adorable_animals",
+            quantidadeLikes: 200.541
+        }
+    ]
+
     return (
         <div class="posts">
-            <div class="post">
-                <div class="topo">
-                    <div class="usuario">
-                        <img src="assets/img/meowed.svg" alt="meowed" />
-                        meowed
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="conteudo">
-                    <img src="assets/img/gato-telefone.svg" alt="gato-telefone" />
-                </div>
-
-                <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="curtidas">
-                        <img src="assets/img/respondeai.svg" alt="respondeai" />
-                        <div class="texto">
-                            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="post">
-                <div class="topo">
-                    <div class="usuario">
-                        <img src="assets/img/barked.svg" alt="barked" />
-                        barked
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="conteudo">
-                    <img src="assets/img/dog.svg" alt="dog" />
-                </div>
-
-                <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="curtidas">
-                        <img src="assets/img/adorable_animals.svg" alt="adorable_animals" />
-                        <div class="texto">
-                            Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {postsArray.map((post) => (
+                <ArrayPost
+                    nomeDoUsuario={post.nomeDoUsuario}
+                    imgPerfil={post.imgPerfil}
+                    imgPost={post.imgPost}
+                    imgcurtidoPor={post.imgcurtidoPor}
+                    curtidoPorTexto={post.curtidoPorTexto}
+                    quantidadeLikes={post.quantidadeLikes}
+                />)
+            )}
         </div>
     )
 }
