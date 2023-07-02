3390c81b35822c2408b259f15f2c1cc9b0be4ae0
Gabriel Dos Santos Monteiro <bhcmonteiro54@gmail.com>
2023-07-02 10:07:18 -0300

    Requisito de renderizar o array usando map pronto

--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,4 +1,29 @@
+function ArraySugestao(props) {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={props.image} alt="bad.vibes.memes.svg" />
+                <div class="texto">
+                    <div class="nome">{props.name}</div>
+                    <div class="razao">{props.segueVoce}</div>
+                </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+        </div>
+    )
+}
+
 export default function Suggestions() {
+
+    const sugestoes = [
+        { name: "bad.vibes.memes", image: "assets/img/bad.vibes.memes.svg", segueVoce: "Segue você" },
+        { name: "chibirdart", image: "assets/img/chibirdart.svg", segueVoce: "Segue você" },
+        { name: "razoesparaacreditar", image: "assets/img/razoesparaacreditar.svg", segueVoce: "Novo no Instagram" },
+        { name: "adorable_animals", image: "assets/img/adorable_animals.svg", segueVoce: "Segue você" },
+        { name: "smallcutecats", image: "assets/img/smallcutecats.svg", segueVoce: "Segue você" }
+    ]
+
     return (
         <div class="sugestoes">
             <div class="titulo">
@@ -6,65 +31,13 @@ export default function Suggestions() {
                 <div>Ver tudo</div>
             </div>
 
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg" />
-                    <div class="texto">
-                        <div class="nome">bad.vibes.memes</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/chibirdart.svg" alt="chibirdart" />
-                    <div class="texto">
-                        <div class="nome">chibirdart</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/razoesparaacreditar.svg" alt="razoesparaacreditar" />
-                    <div class="texto">
-                        <div class="nome">razoesparaacreditar</div>
-                        <div class="razao">Novo no Instagram</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/adorable_animals.svg" alt="adorable_animals" />
-                    <div class="texto">
-                        <div class="nome">adorable_animals</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/smallcutecats.svg" alt="smallcutecats" />
-                    <div class="texto">
-                        <div class="nome">smallcutecats</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
+            {sugestoes.map((sugestao) => (
+            <ArraySugestao 
+            name ={sugestao.name} 
+            image={sugestao.image} 
+            segueVoce={sugestao.segueVoce} />
+            )
+            )}
         </div>
     )
 }
